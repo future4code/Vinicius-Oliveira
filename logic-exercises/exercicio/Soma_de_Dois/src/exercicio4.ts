e5f6cb85c181a09f7b8ca7f89a0e3ed01bfa3300
vinnivso <vinnivsob@gmail.com>
2021-12-02 12:05:14 -0300

    Logic Exercises

---
+++ logic-exercises/exercicio/Soma_de_Dois/src/exercicio4.ts
@@ -0,0 +1,28 @@
+/**
+ * ### Exercício do dia - Soma de dois
+
+ ## Exercício de lógica - **Soma de dois**
+
+    Escreva uma função que recebe um array de números `nums` e um `target` e retorna os **indíces** dos dois elementos que somam o valor do **target**.
+
+    Você pode assumir que qualquer input possível sempre terá **exatamente** 1 solução, e você não pode usar o mesmo elemento duas vezes.
+
+  - **Desafio**
+    Resolva o problema usando uma complexidade linear de tempo (O(N)). Isso significa que você só deve iterar o array uma vez para encontrar o número.
+
+ - Exemplo 1
+
+    **Input:** `nums=[2, 7, 11, 15]` `target=9`
+
+    **Output:** `[0,1]`
+
+    **Explicação:** O elemento no index 0 (2) somado com o elemento no index 1 (7) dão 9, que é o *target*
+
+ - Exemplo 2
+
+    **Input:** `nums=[4, 5, 10, 12, 21]` `target=17`
+
+    **Output:** `[1,3]`
+
+    **Explicação:** O elemento no index 1 (5) somado com o elemento no index 3 (12) dão 17, que é o *target*
+ */
